a70f25be11b9a31bb66c1df78d4500c7c294f075
Makale Lord <110202582+MakaleLord@users.noreply.github.com>
2022-07-28 17:36:14 -0500

    initial commit

---
+++ facts.py
@@ -0,0 +1,68 @@
+<style>
+@import url('https://fonts.googleapis.com/css?family=Roboto+Mono');
+html{
+   display:flex;
+   justify-content:center;
+   align-items: center;
+   width: 100%;
+   height: 100%;
+   background-image: url('https://source.unsplash.com/collection/327760/1600x900');
+   background-size:cover;
+}
+
+body{
+   min-width:55%;
+   max-width:100%;
+   height:60vh;
+   border:1px solid lightgray;
+   background-color:white;
+   border-radius:8px;
+   font-family: 'Roboto Mono', monospace;
+   box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
+   border-top:32px solid #eee;
+   overflow-y:scroll;
+}
+
+body > div{
+  height: calc(100% - 40px);
+  overflow-y:auto;
+  padding: 20px;
+  white-space: pre;
+}
+</style>
+
+<div>
+Content-type:text/html 
+
+Celebrity facts
+
+Celebrity facts about Robert Downy Jr
+- Robert Downy Jr was born in 1965                
+- Robert Downy Jr first movie was Pound.
+- Robert Downy Jr has a rating of 6.3
+
+Celebrity facts
+
+Celebrity facts about Chris Hemsworth
+- Chris Hemsworth was born in 1983                
+- Chris Hemsworth first movie was Star Trek.
+- Chris Hemsworth has a rating of not provided
+
+Celebrity facts
+
+Celebrity facts about Mark Ruffalo
+- Mark Ruffalo was born in 1967                
+- Mark Ruffalo first movie was The Denist.
+- Mark Ruffalo has a rating of 5.2
+
+Celebrity facts
+
+Celebrity facts about Leonardo DiCaprio
+- Leonardo DiCaprio was born in 1974                
+- Leonardo DiCaprio first movie was This Boy's Life.
+- Leonardo DiCaprio has a rating of 7.3
+
+  Movie name: Avengers: Infinity war IMdB: 8.5 Rotten Tomatoes: -- 
+  Movie name: Doctor Strange IMdB: 7.5 Rotten Tomatoes: 89% 
+  Movie name: The Pound IMdB: -- Rotten Tomatoes: -- 
+  Movie name: Guardians of The Galaxy IMdB: 8.1 Rotten Tomatoes: 91% 
---
+++ table.py
@@ -0,0 +1,67 @@
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Roboto+Mono');
+html{
+   display:flex;
+   justify-content:center;
+   align-items: center;
+   width: 100%;
+   height: 100%;
+   background-image: url('https://source.unsplash.com/collection/327760/1600x900');
+   background-size:cover;
+}
+
+body{
+   min-width:55%;
+   max-width:100%;
+   height:60vh;
+   border:1px solid lightgray;
+   background-color:white;
+   border-radius:8px;
+   font-family: 'Roboto Mono', monospace;
+   box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
+   border-top:32px solid #eee;
+   overflow-y:scroll;
+}
+
+body > div{
+  height: calc(100% - 40px);
+  overflow-y:auto;
+  padding: 20px;
+  white-space: pre;
+}
+</style>
+
+<div>
+7 x 1 = 7
+7 x 2 = 14
+7 x 3 = 21
+7 x 4 = 28
+7 x 5 = 35
+7 x 6 = 42
+7 x 7 = 49
+7 x 8 = 56
+7 x 9 = 63
+7 x 10 = 70
+
+10 x 1 = 10
+10 x 2 = 20
+10 x 3 = 30
+10 x 4 = 40
+10 x 5 = 50
+10 x 6 = 60
+10 x 7 = 70
+10 x 8 = 80
+10 x 9 = 90
+10 x 10 = 100
+
+1 x 1 = 1
+1 x 2 = 2
+1 x 3 = 3
+1 x 4 = 4
+1 x 5 = 5
+1 x 6 = 6
+1 x 7 = 7
+1 x 8 = 8
+1 x 9 = 9
+1 x 10 = 10
